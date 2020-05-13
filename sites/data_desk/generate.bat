e90ef97a96d6d802895975ac0464b40711850f25
ryanfleury <ryan.j.fleury@gmail.com>
2020-05-13 13:11:35 -0600

    Update canonical URL for Data Desk site

--- sites/data_desk/generate.bat
+++ sites/data_desk/generate.bat
@@ -8,4 +8,4 @@ start /b "" "xcopy" .\data .\generated\data\ /y /s /e /q
 start /b /wait "" "xcopy" search.js generated\ /y
 set files=
 for %%i in (*.rxw) do ( call set "files=%%files%% %%i" )
-..\..\generator\build\generator.exe --main_title "Data Desk" --author "Ryan Fleury" --canonical_url "https://datadesk.net" --twitter_handle "@ryanjfleury" --html --html_header header.html --html_footer footer.html %files% custom_layer_api.html
+..\..\generator\build\generator.exe --main_title "Data Desk" --author "Ryan Fleury" --canonical_url "https://data-desk.net" --twitter_handle "@ryanjfleury" --html --html_header header.html --html_footer footer.html %files% custom_layer_api.html
