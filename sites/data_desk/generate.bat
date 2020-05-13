c701f0a9ca523533926ee445d15d911b48086629
Ryan Fleury <ryan.j.fleury@gmail.com>
2020-05-12 18:28:43 -0600

    Data Desk website and generator tweaks

---
+++ sites/data_desk/generate.bat
@@ -0,0 +1,11 @@
+@echo off
+
+rem --- Make Directories
+if not exist generated mkdir generated
+
+rem --- Generate HTML
+start /b "" "xcopy" .\data .\generated\data\ /y /s /e /q
+start /b /wait "" "xcopy" search.js generated\ /y
+set files=
+for %%i in (*.rxw) do ( call set "files=%%files%% %%i" )
+..\..\generator\build\generator.exe --main_title "Data Desk" --author "Ryan Fleury" --canonical_url "https://datadesk.net" --twitter_handle "@ryanjfleury" --html --html_header header.html --html_footer footer.html %files% documentation.html
